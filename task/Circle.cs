fca68d2bfb778bb63d7c9af221823cd6806d6690
ipz_242-rdr <ipz242_rdr@student.ztu.edu.ua>
2026-01-15 20:56:40 +0200

    Реалізовано клас Circle з усіма методами та полем для радіусу

---
+++ task/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    public class Circle : Shape
+    {
+        private double _radius;
+        private string _name = "Коло";
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return _radius; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Радіус не може бути від'ємним");
+                _radius = value;
+            }
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"----{_name}----\nПлоща: {CalculateArea()}\nПериметр: {CalculatePerimeter()}\n");
+        }
+    }
+}
--- task/obj/Debug/net8.0/task.AssemblyInfo.cs
+++ task/obj/Debug/net8.0/task.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0a51448a7056e6b8a4df71837a2282e362aec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
